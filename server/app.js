27870e54b317d1777fd16ef2c935ac803de46f63
sofiaIG <sphignatiadi@gmail.com>
2022-07-12 16:50:06 +0100

    Fix url

--- server/app.js
+++ server/app.js
@@ -1,31 +1,15 @@
 
 
-const Customer = require('./customer.js');
-const Order = require('./order.js');
-
-useEffect(()=>{
-    getItem()
-},[])
-
-itemBought = "Wallace"
-const customer = new Customer("John Johnson", "43 Membris Way, Woodford Halse, NN11 3QZ");
-const order = new Order( "10/07/2022", customer, item, 2, 01)
-
-const getItem = function(){
-    fetch(`http://localhost:8080/plants/${itemBought}`)
-        .then(res =>res.json())
-        .then(result =>setItem(result))
-
-}
 
 const submitMessage = (order) => {
     const orderBody = {
-      order_number: 'hof',
-      plant: order.item,
-      number_purchased: order.quantity,
-      shipping_address: order.customer.address
+      orderNumber: '1002',
+      plant: 1,
+      quantity: 2,
+      shippingAddress:"10 London Street, Eh7 8IK",
+      customerName: "Kate Johnson"
     };
-    fetch("http://localhost:8080/orders", {
+    fetch("http://localhost:5000/orders", {
       method: "POST",
       body: JSON.stringify(orderBody),
       headers: { "content-type": "application/json" },
@@ -33,7 +17,7 @@ const submitMessage = (order) => {
       .then((res) => res.json())
   };
 
-export {customer, order}
+
 
 
 
